183ac236820935b4770d469656a74cb5cf147950
Flayka <the.flayka@gmail.com>
2026-07-05 14:04:07 +0500

    Added Hono framework

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Check ENV variables
+import { portNum } from './shared/env.js'
+
+// Hono
+import { Hono } from 'hono'
+import type { HonoBase } from 'hono/hono-base'
+const app: HonoBase = new Hono()
+
+// Main route
+app.get('/', (c) => c.text('It is API of DTHW Backend!'))
+
+export default {
+	port: portNum,
+	fetch: app.fetch,
+}
---
+++ src/shared/env.ts
@@ -0,0 +1,7 @@
+// Port var
+const portNum = parseInt(Bun.env.PORT || "3000", 10);
+if (isNaN(portNum) || portNum < 0 || portNum > 65535) {
+  console.error(`[CRITICAL, ${new Date().toISOString()}] Invalid PORT value: ${Bun.env.PORT}`)
+  process.exit(1)
+}
+export { portNum }
